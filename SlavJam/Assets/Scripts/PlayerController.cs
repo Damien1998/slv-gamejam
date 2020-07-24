60a9b3e15e6ac5988a942bb8cdbf7854df64387a
Ortuy <60518287+Ortuy@users.noreply.github.com>
2020-07-24 21:07:01 +0200

    Absolutna Podstawa
    Gracz idzie cały czas do przodu
    Może spowalniać i zmieniać tory
    Tor bez niczego generuje się w nieskończoność

---
+++ SlavJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float fastSpeed, slowSpeed;
+    private Rigidbody rigidBody;
+
+    private float moveSpeed;
+
+    public static PlayerController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        rigidBody = GetComponent<Rigidbody>();
+        moveSpeed = fastSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("SlowDown"))
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, slowSpeed, 0.4f);
+        }
+        else
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, fastSpeed, 0.6f);
+        }
+
+        rigidBody.velocity = new Vector3(moveSpeed, 0, 0);
+
+        if (Input.GetKeyDown("w") && transform.position.z < 1)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 1);
+        }
+
+        if (Input.GetKeyDown("s") && transform.position.z > -1)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z - 1);
+        }
+    }
+}
---
+++ SlavJam/Assets/Scripts/TrackRemover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackRemover : MonoBehaviour
+{
+    public GameObject previousTrack, currentTrack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && previousTrack != null)
+        {
+            Destroy(previousTrack);
+            float distance = -currentTrack.transform.position.x;
+            currentTrack.transform.position = new Vector3(0, currentTrack.transform.position.y, currentTrack.transform.position.z);
+            PlayerController.instance.transform.position = new Vector3(PlayerController.instance.transform.position.x + distance, PlayerController.instance.transform.position.y, PlayerController.instance.transform.position.z);
+        }
+    }
+}
