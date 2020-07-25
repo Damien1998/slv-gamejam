a01711f1a6a349d2faaf04375d0ad1dd54ec2e10
Ortuy <60518287+Ortuy@users.noreply.github.com>
2020-07-25 12:17:55 +0200

    Krzakowanko
    knjbhhuvgytcrtrxwe

---
+++ SlavJam/Assets/EnemyRedirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRedirector : MonoBehaviour
+{
+    public int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + direction, other.transform.position.z + direction);
+            other.GetComponent<SpriteRenderer>().sortingOrder -= direction;
+        }
+    }
+}
